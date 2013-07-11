2967cdc955537c2f458fd54ab61e32333ef15bcd
Elliot Shepherd <elliot@jarofworms.com>
2013-07-11 16:48:13 +1000

    Expand the light driver to support plain on/off and dimmable zigbee lights

--- devices/Light.js
+++ devices/Light.js
@@ -10,9 +10,15 @@ function Light(address, headers, zigbeeDevice, socket) {
     this.writable = true;
     this.V = 0;
     this.D = 224; // Light
-
-    this.write({hue:12,sat:212,bri:99,on:true,transitionTime:50});
 }
+
+Light.prototype.hasLevelControl = function() {
+    return this.hasServerCluster('Level Control');
+};
+
+Light.prototype.hasColorControl = function() {
+    return this.hasServerCluster('Color Control');
+};
 
 Light.prototype.write = function(data) {
 
@@ -20,22 +26,29 @@ Light.prototype.write = function(data) {
         data = JSON.parse(data);
     }
 
-    var hue = data.hue >> 8;
-
-    this.sendCommand(P.RPCS_SET_DEV_COLOR, function(msg) {
-        msg.UInt8(hue);
-        msg.UInt8(data.sat);
-        msg.UInt16LE(data.transitionTime || 10);
-    });
-
-    this.sendCommand(P.RPCS_SET_DEV_LEVEL, function(msg) {
-        msg.UInt8(data.bri);
-        msg.UInt16LE(data.transitionTime || 10);
-    });
+    this.log.info('Setting to ' + JSON.stringify(data));
 
     this.sendCommand(P.RPCS_SET_DEV_STATE, function(msg) {
         msg.UInt8(data.on? 0xFF : 0x0);
     });
+
+    if (this.hasLevelControl()) {
+        this.sendCommand(P.RPCS_SET_DEV_LEVEL, function(msg) {
+            msg.UInt8(data.bri);
+            msg.UInt16LE(data.transitionTime || 10);
+        });
+    }
+
+    if (this.hasColorControl()) {
+        var hue = data.hue >> 8;
+
+        this.sendCommand(P.RPCS_SET_DEV_COLOR, function(msg) {
+            msg.UInt8(hue);
+            msg.UInt8(data.sat);
+            msg.UInt16LE(data.transitionTime || 10);
+        });
+    }
+
 };
 
 module.exports = Light;
