a94f66603107798920c1de6b85ec74b74b3657c4
Elliot Shepherd <elliot@jarofworms.com>
2013-08-28 12:03:09 +1000

    On/off switch binding

--- devices/OnOffSwitch.js
+++ devices/OnOffSwitch.js
@@ -4,8 +4,6 @@ var P = require('../lib/protocol');
 
 util.inherits(OnOffSwitch, Device);
 
-// TODO: UNTESTED
-
 // TODO: Support cluster "Level Control" for device "Level Control Switch"
 function OnOffSwitch(address, headers, zigbeeDevice, socket) {
     OnOffSwitch.super_.apply(this, arguments);
@@ -14,7 +12,9 @@ function OnOffSwitch(address, headers, zigbeeDevice, socket) {
     this.V = 0;
     this.D = 244; // state device
 
-    this.onCommand(P.RPCS_GET_DEV_STATE_RSP, function(address, reader) {
+    this.bindToCluster('On/Off');
+
+    this.onCommand(P.SRPC_ONOFF_CMD, function(address, reader) {
         reader.word8('value');
 
         this.log.debug('State change value : ', reader.vars.value);
